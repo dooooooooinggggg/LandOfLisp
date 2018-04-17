eac801efab0e613c20b43539764038481e66530a
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-04-17 17:34:14 +0900

    トップレベル変数で、場所の描画

---
+++ text_game/13_game.lisp
@@ -0,0 +1,9 @@
+
+;; トップレベル変数で、場所の描画
+(defparameter *nodes* '(
+    (living-room (you are in the living room.
+        a wizard is snoring loudlu on the couch.))
+    (garden (you are in a beautiful garden.
+        ttherere is a well in vront of you))
+    (attic (you are in the attic.
+        there is a giant welding torch in the corner))))
