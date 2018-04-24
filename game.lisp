d21825eb3d0b3749f38ab1d21558888b10f73aa0
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-04-24 14:22:07 +0900

    bug fix 2

--- game.lisp
+++ game.lisp
@@ -45,12 +45,12 @@
 (defun objects-at (loc objs obj-locs)
         (labels ((at-loc-p (obj)
                     (eq (cadr (assoc obj obj-locs)) loc)))
-                (remove if-not #'at-loc-p objs)))
+                (remove-if-not #'at-loc-p objs)))
 
 (defun describe-objects (loc objs obj-loc)
     (labels ((describe-obj (obj)
-                `(you see a ,obj on the floor.))
-            (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc))))))
+                `(you see a ,obj on the floor.)))
+        (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
 
 (defun look ()
     (append
