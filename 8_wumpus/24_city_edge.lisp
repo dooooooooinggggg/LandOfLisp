34d84c402ddb9d12866ce1588ef0a469330174a7
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-04-25 15:58:18 +0900

    add graphutil and init file

---
+++ 8_wumpus/24_city_edge.lisp
@@ -0,0 +1,12 @@
+;; Use "Graph util" created in Chaptar7
+(load "25_graph_util")
+
+(defparameter *congestion-city-nodes* nil)
+(defparameter *congestion-city-edges* nil)
+(defparameter *visited-nodes* nil)
+(defparameter *node-num* 30)
+(defparameter *edge-num* 45)
+(defparameter *worm-num* 3)
+(defparameter *cop-odds* 15)
+
+
