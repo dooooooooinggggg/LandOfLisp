e90e8ddc28bafc98175cba8456d51d373785e6a4
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-05-04 18:24:21 +0900

    update

--- 8_wumpus/gtw.lisp
+++ 8_wumpus/gtw.lisp
@@ -170,6 +170,36 @@
     (draw-city)
     (draw-known-city))
 
+(defun walk (pos)
+    (handle-direction pos nil))
 
+(defun charge (pos)
+    (handle-direction pos t))
+
+(defun handle-direction (pos charging)
+    (let ((edge (assoc pos
+                    (cdr (assoc *player-pos* *congestion-city-edges*)))))
+        (if edge
+            (handle-new-place edge pos charging)
+            (princ "That location doen not exsit!!"))))
+
+(defun handle-new-place (edge pos charging)
+    (let* (
+            (node (assoc pos *congestion-city-nodes*))
+            (has-worm (and
+                    (member 'glow-worm node)
+                    (not (member pos *visited-nodes*)))))
+        (pushnew pos *visited-nodes*)
+        (setf *player-pos* pos)
+        (draw-known-city)
+        (cond ((member 'cops edge) (princ "You ran into the cops. Game Over."))
+            ((member 'wumpus node) (if charging
+                    (princ "You found the Wumpus!")
+                    (princ "You ran into the Wumpus")))
+            (charging (princ "You wasted  your last bullet. Game Over."))
+            (has-worm (let ((new-pos (random-node)))
+                    (princ "You ran into a Glow Worm Gang! You are now at ")
+                    (princ new-pos)
+                    (handle-new-place nil new-pos nil))))))
 
 (new-game)
