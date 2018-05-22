b07a42de6ed8ee8048913450a2166b584c305be0
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-05-23 01:12:37 +0900

    一旦完成(エラーはまだある)

--- Practice/lifegame.lisp
+++ Practice/lifegame.lisp
@@ -4,7 +4,7 @@
 (defparameter *height* 100)
 
 (defparameter *prev_val* (make-array (* *width* *height*)))
-(defparameter *next_val* nil)
+(defparameter *next_val* (make-array (* *width* *height*)))
 
 (defun define_init_val ()
     (loop for i below (*  *width* *height*)
@@ -13,11 +13,14 @@
                 (cond
                     ((not (or (= (mod i *width*) 0) (= (mod i *width*) (- *width* 1)) (< i *width*) (>= i (- (* *width* *height*) *width*))))
                         (setf (aref *prev_val* i) (random 2)))
-                    (t (setf (aref *prev_val* i) 0)))
-            )
-        )
+                    (t (setf (aref *prev_val* i) 0))))))
+    (princ *prev_val*))
+
+(defun cp-next-to-prev ()
+    (loop for i below (* *width* *height*)
+        do ((lambda ()
+            (setf (aref *prev_val* i) (aref *next_val* i))))
     )
-    (princ *prev_val*)
 )
 
 (defun print-mold ()
@@ -30,8 +33,6 @@
         (fresh-line )))
 
 (defun print-each (i)
-    ;; (princ i)
-    ;; (fresh-line )
     (unless (or (= (mod i *width*) 0) (= (mod i *width*) (- *width* 1)) (< i *width*) (>= i (- (* *width* *height*) *width*)))
         (cond
             ((= (aref *prev_val* i) 1)
@@ -40,14 +41,54 @@
                 (princ " "))
             (t (princ "e")))))
 
-(defun life-span ()
+;; ・そのマスに生命体が存在し、周囲8マスに生命体が2体または3体存在するならば生存。
+;; ・そのマスに生命体が存在し、周囲8マスに生命体が1体以下または4体以上存在するならば死滅。
+;; ・そのマスに生命体が存在せず、周囲8マスに生命体が3体存在するならば誕生。
+
+(defun next-gen (i)
+    (let ((c 0))
+        (unless (or (= (mod i *width*) 0) (= (mod i *width*) (- *width* 1)) (< i *width*) (>= i (- (* *width* *height*) *width*)))
+            (setf c
+                (
+                    (lambda (counter-list)
+                        (apply #'+ counter-list)
+                    )
+                    (list
+                        (aref *prev_val* (- i 101))
+                        (aref *prev_val* (- i 100))
+                        (aref *prev_val* (- i 99))
+                        (aref *prev_val* (- i 1))
+                        (aref *prev_val* (+ i 1))
+                        (aref *prev_val* (+ i 99))
+                        (aref *prev_val* (+ i 100))
+                        (aref *prev_val* (+ i 101)))))
+            (cond
+                ((= (aref *prev_val* 1))
+                    (cond
+                        ((or (= c 2) (= c 3))
+                            (setf (aref *next_val* i) 1))
+                        (t (setf (aref *next_val* i) 0))))
+                ((= (aref *prev_val* 0))
+                    (cond
+                        ((= c 3)
+                            (setf (aref *next_val* i) 1))
+                        (t (setf (aref *next_val* i) 0))))))))
+
+(defun new-gen()
+    (loop for i below (* *width* *height*)
+        do (next-gen i)))
+
+(defun life-span (gen)
     (sleep 1)
-    (print-mold))
+    (princ (format nil "第~d世代" gen))
+    (fresh-line )
+    (print-mold)
+    (new-gen)
+    (cp-next-to-prev))
 
 (defun main()
     (define_init_val)
     (loop for i below *lifespan*
-        do (life-span)
-        ))
+        do (life-span i)))
 
 (main)
