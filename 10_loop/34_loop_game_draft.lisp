d0f67039e8f553162f1ca4120030be822922140f
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-05-22 18:29:58 +0900

    update

--- 10_loop/34_loop_game_draft.lisp
+++ 10_loop/34_loop_game_draft.lisp
@@ -27,10 +27,10 @@
 (defun move (animal)
     (let ((dir (animal-dir animal))
             (x (animal-x animal))
-            (y (animal-y animal))
+            (y (animal-y animal)))
         (setf (animal-x animal) (mod (+ x
                     (cond ((and (>= dir 2) (< dir 5)) 1)
-                        ((or (= dir 1) (= dir 5) 0))
+                        ((or (= dir 1) (= dir 5)) 0)
                         (t -1)))
                 *width*))
         (setf (animal-y animal) (mod (+ y
@@ -38,10 +38,10 @@
                         ((and (>= dir 4) (< dir 7)) 1)
                         (t 0)))
                 *height*))
-        (decf (animal-energy animal)))))
+        (decf (animal-energy animal))))
 
 (defun turn (animal)
-    (let ((x (random (apply '#+ (animal-genes animal)))))
+    (let ((x (random (apply #'+ (animal-genes animal)))))
         (labels ((angle (genes x)
                     (let ((xnu (- x (car genes))))
                         (if (< xnu 0)
@@ -56,3 +56,61 @@
         (when (gethash pos *plants*)
             (incf (animal-energy animal) *plants*)
             (remhash pos *plants*))))
+
+(defparameter *reproduction-energy* 200)
+
+(defun reproduce (animal)
+    (let ((e (animal-energy animal)))
+        (when (>= e *reproduction-energy*)
+            (setf (animal-energy animal) (ash e -1))
+            (let ((animal-nu (copy-structure animal))
+                    (genes (copy-list (animal-genes animal)))
+                    (mutation (random 8)))
+                (setf (nth mutation genes) (max 1 (+ (nth mutation genes) (random 3) -1)))
+                (setf (animal-genes animal-nu) genes)
+                (push animal-nu *animals*)))))
+
+(defun update-world ()
+    (setf *animals* (remove-if (lambda (animal)
+                (<= (animal-energy animal) 0))
+            *animals*))
+        (mapc (lambda (animal)
+                (turn animal)
+                (move animal)
+                (eat animal)
+                (reproduce animal))
+            *animals*)
+        (add-plants))
+
+(defun draw-world ()
+    (loop for y
+        below *height*
+        do (progn (fresh-line )
+            (princ "|")
+            (loop for x
+                below *width*
+                do (princ (cond ((some (lambda (animal)
+                                    (and (= (animal-x animal) x)
+                                        (= (animal-y animal) y)))
+                                *animals*)
+                            #\M)
+                        ((gethash (cons x y) *plants*) #\*)
+                        (t #\space))))
+            (princ "|"))))
+
+(defun evolution ()
+    (draw-world)
+    (fresh-line )
+    (let ((str (read-line )))
+        (cond ((equal str "quit") ())
+            (t (let ((x (parse-integer str :junk-allowed t)))
+                    (if x
+                        (loop for i
+                            below x
+                            do (update-world)
+                            if (zerop mod(mod i 1000))
+                            do (princ #\.))
+                        (update-world))
+                    (evolution))))))
+
+(evolution)
